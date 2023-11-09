448c78d4429a92396d47fc6d04b69a895b7b2e48
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-09 11:12:59 +0530

    Initial Commit

---
+++ firebase-Swift_bootcamp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  firebase-Swift_bootcamp
+//
+//  Created by Akhil on 09/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firebase-Swift_bootcamp/firebase_Swift_bootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_Swift_bootcampApp.swift
+//  firebase-Swift_bootcamp
+//
+//  Created by Akhil on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct firebase_Swift_bootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
